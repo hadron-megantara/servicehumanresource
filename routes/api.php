3160e137e677d02b45081be548a557875024b0f5
Hadron Megantara <hadron@Macs-MacBook-Pro.local>
2018-08-15 13:53:10 +0700

    Add User Permission

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,31 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(array('prefix' => 'v1'), function(){
+    Route::post('/login', 'Api\LoginController@login');
+
+    Route::middleware(['auth:api'])->group(function () {
+        Route::group(array('prefix' => 'master'), function(){
+            Route::get('/province', 'MasterController@getProvince');
+            Route::get('/district', 'MasterController@getDistrict');
+            Route::get('/province', 'MasterController@getProvince');
+        });
+
+        Route::group(array('prefix' => 'config'), function(){
+            Route::get('/menu', 'ConfigController@getMenu');
+            Route::get('/permission', 'ConfigController@permission');
+            Route::get('/roles', 'ConfigController@roles');
+            Route::get('/users', 'ConfigController@users');
+        });
+
+        Route::group(array('prefix' => 'user'), function(){
+            Route::get('profile', 'UserController@getProfile');
+            Route::get('profile/photo', 'UserController@photoUpload');
+        });
+
+        Route::resource('/outlet', 'Api\OutletController');
+        // Route::post('/outlet/{id}', 'Api\OutletController@upload');
+    });
+
+});
